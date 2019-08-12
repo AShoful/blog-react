0157bc509ef7881890d2d7b419e7000ff17e2283
Andrey Shoful <shoful.a@gmail.com>
2019-08-12 13:04:19 +0300

    Create component Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import modules from './App.module.css';
+import Layout from './hoc/Layout/Layout'
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div className={modules.App}>
-      <header className={modules.AppHeader}>
-        <img src={logo} className={modules.AppLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={modules.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <h1> Blog-react </h1>
+    </Layout>
   );
 }
 
