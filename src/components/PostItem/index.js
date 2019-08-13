75f5dcf442e8f5411d5cd630dca834ebea344489
Andrey Shoful <shoful.a@gmail.com>
2019-08-13 15:19:25 +0300

    Add components/MenuToggle

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import  classes from './PostItem.module.css'
 
-const PostItem = ({title, createdAt}) => 
-		<div className = {classes.PostItem}>
+const PostItem = ({title, createdAt, _id}) => {
+	return(
+	<div className = {classes.PostItem} 
+		onClick = {() => console.log(_id)}>
       <p className={classes.PostItemText}>{title}</p>
       <p>{new Date(createdAt).toLocaleString().split(", ")[0]}</p>                
-    </div>
+    </div>)}
 
 PostItem.defaultProps = {
-  title: 'Blog React',
-  createdAt: Date.now()};
+	title: 'Blog React',
+	createdAt: Date.now(),
+	_id: 1234
+};
 
 export default PostItem;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 import HeaderBlock from '../../components/HeaderBlock/'
+import MenuToggle from '../../components/Navigation/MenuToggle'
 import classes from './Layout.module.css'
 
 
 class Layout extends Component {
+	state = {
+		menu: false
+	}
+
+	toggleMenuHandler = () =>{
+		 this.setState({
+	      menu: !this.state.menu
+	    })
+	}
 
 	render (){
 		return (
 			<div className={classes.Layout}>
 			<HeaderBlock />
+			<MenuToggle
+	          onToggle={this.toggleMenuHandler}
+	          isOpen={this.state.menu}
+	        />
 				<main>
 					{this.props.children}
 				</main>
