47dfcda574c83aa18abd1cdfb67cf680d46ffa4c
Andrey Shoful <shoful.a@gmail.com>
2019-08-14 16:09:06 +0300

    added icon goback FullPost

--- src/components/FullPost/index.js
+++ src/components/FullPost/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom'
 
 import classes from './FullPost.module.css';
 
-const FullPost = ({ text, createdAt }) => (
-  
+const FullPost = ({text, createdAt, history:{goBack}}) => 
+	 
 	<div className={classes.FullPost}>
-	   <div className={classes.details}>
-	       <p>{new Date(createdAt).toLocaleString().split(", ")[0]}</p>
-	    </div>
+		<div className={classes.details}>
+			<i className="fas fa-angle-double-left" onClick = {goBack}></i>	
+		    <p>{new Date(createdAt).toLocaleString().split(", ")[0]}</p>
+		</div>
 	    <p className={classes.text}>{text}</p>
 	</div>
   
-);
+
 
 FullPost.defaultProps = {
 	text: 'Гаагский суд опубликовал позиции Украины и России относительно юрисдикции суда по делу о правах прибрежного государства в Черном и Азовском морях и в Керченском проливе.\nОб этом сообщает пресс-служба Министерства внутренних дел Украины, передает Цензор.НЕТ.\n\nКак сообщается, 22 мая 2018 года РФ подала свои возражения относительно наличия у суда юрисдикции рассматривать дело, инициированное Украиной по Конвенции ООН по морскому праву. После этого Украина подала свои письменные объяснения и требования. Источник: https://censor.net.ua/n3131510',
@@ -19,4 +21,4 @@ FullPost.defaultProps = {
 	
 };
 
-export default FullPost;
+export default withRouter(FullPost);
