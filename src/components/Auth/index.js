c1a02f281167d6c91b1b7a4ebf588b45fd933055
Andrey Shoful <shoful.a@gmail.com>
2019-08-15 14:41:09 +0300

    added component/UI/Input

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import classes from './Auth.module.css'
 import Button from '../UI/Button/'
+import Input from '../UI/Input'
 
 const Auth = () => 
-	<div className = {classes.Auth}>
-		<Button>На главную</Button>
-		<Button type = 'success'>Отправить</Button>
-		<Button type = 'error'>Зарегистрироваться</Button>
-		<Button disabled >Auth</Button>
-	</div>
+	<React.Fragment>
+		<div className = {classes.Auth}>
+			<Button>На главную</Button>
+			<Button type = 'success'>Отправить</Button>
+			<Button type = 'error'>Зарегистрироваться</Button>
+			<Button disabled >Auth</Button>
+		</div>
+		<div className = {classes.Auth}>
+			<Input 
+				label = 'Пароль'
+				type = 'text'
+				size = 'middle'
+			/>
+			<Input 
+				label = 'Пароль'
+				type = 'text'
+				size = 'middle'
+			/>
+			<Input 
+				label = 'Пароль'
+				type = 'text'
+				size = 'big'
+			/>
+		</div>
+	</React.Fragment>
 
 
 export default Auth
