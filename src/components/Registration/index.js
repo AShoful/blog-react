57f1a0202fed3f6ab7ac42a6952cf854c1a95e54
Andrey Shoful <shoful.a@gmail.com>
2019-08-25 13:26:30 +0300

    changed components/Registration using React hooks

--- src/components/Registration/index.js
+++ src/components/Registration/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import classes from './Registration.module.css'
 import { Link } from 'react-router-dom'
 import RegistValidation from './RegistValidation.js'
@@ -6,30 +6,18 @@ import postApi from '../../api/'
 import Button from '../UI/Button/'
 import Input from '../UI/Input'
 
-class Registration extends React.Component{
-	state = {
-		user: {
-                firstName: '',
-                login: '',
-                password: '',
-                passwordConfirm: ''
-           },
-        touched: false   
-	} 
+const Registration = () => {
 
-	handleChange = (e) => {
-        const { name, value } = e.target;
-        const { user } = this.state;
-        this.setState({
-            user: {
-                ...user,
-                [name]: value
-            },
-            touched: true
-        });
-    }
+	const [firstName, setFirstName] = useState('')
+	const [login, setLogin] = useState('')
+	const [password, setPassword] = useState('')
+	const [passwordConfirm, setPasswordConfirm] = useState('')
+	const [touched, setTouched] = useState(false) 
 
-    handleSubmit =  async (data, e) => {
+	 
+	const warning = RegistValidation({firstName, login, password, passwordConfirm})
+
+	const handleSubmit =  async (data, e) => {
         e.preventDefault();
         
         const res = await postApi.postUser(data)
@@ -37,14 +25,11 @@ class Registration extends React.Component{
               const { error, ok } = res.data
               if(ok){
                   window.alert("Регистрация прошла успешно")
-                  this.setState({
-                    user: {
-                        firstName: '',
-                        login: '',
-                        password: '',
-                        passwordConfirm: ''
-                    }
-                })
+                  setLogin('')
+                  setPassword('')
+                  setFirstName('')
+                  setPasswordConfirm('')
+                  setTouched(false)
               } else {
                   window.alert(error)
               }
@@ -52,9 +37,7 @@ class Registration extends React.Component{
            catch (err){console.log(err)}
     }
 
-	render(){
-		const {user:{firstName, login, password, passwordConfirm}, touched} = this.state
-		const warning = RegistValidation(this.state.user)
+
 		return (
 				<form className = {classes.Registration} action="" method="post">
 					<fieldset className = {classes.fieldset}> 
@@ -65,14 +48,14 @@ class Registration extends React.Component{
 							name = 'firstName'
 							size = 'middle'
 							value = {firstName}
-							onChange = {this.handleChange}
+							onChange = {e => {setFirstName(e.target.value); setTouched(true)}}
 						/>
 						<Input 
 							label = 'Логин'
 							name = 'login'
 							size = 'middle'
 							value = {login}
-							onChange = {this.handleChange}
+							onChange = {(e) => {setLogin(e.target.value); setTouched(true)}}
 						/>
 						<Input 
 							label = 'Пароль'
@@ -80,7 +63,7 @@ class Registration extends React.Component{
 							name = 'password'
 							size = 'middle'
 							value = {password}
-							onChange = {this.handleChange}
+							onChange = {(e) => {setPassword(e.target.value); setTouched(true)}}
 						/>
 						<Input 
 							label = 'Повторите пароль'
@@ -88,7 +71,7 @@ class Registration extends React.Component{
 							name = 'passwordConfirm'
 							size = 'middle'
 							value = {passwordConfirm}
-							onChange = {this.handleChange}
+							onChange = {(e) => {setPasswordConfirm(e.target.value); setTouched(true)}}
 						/>
 					</div>
 					{touched ? <span>{warning}</span> : null}
@@ -96,7 +79,7 @@ class Registration extends React.Component{
 					<div className = {classes.wrap}>
 						<Button 
 							disabled = {!!warning}
-							onClick = {this.handleSubmit.bind(this, {login, firstName, password})}>
+							onClick = {(e) => handleSubmit({login, firstName, password}, e) } >
 							Зарегистрироваться
 						</Button>
 						<Link to ='/auth'>
@@ -104,8 +87,109 @@ class Registration extends React.Component{
 						</Link>	
 					</div>
 				</form>
-		)}
-}
+		)
+	}
+
+// class Registration extends React.Component{
+// 	state = {
+// 		user: {
+//                 firstName: '',
+//                 login: '',
+//                 password: '',
+//                 passwordConfirm: ''
+//            },
+//         touched: false   
+// 	} 
+
+// 	handleChange = (e) => {
+//         const { name, value } = e.target;
+//         const { user } = this.state;
+//         this.setState({
+//             user: {
+//                 ...user,
+//                 [name]: value
+//             },
+//             touched: true
+//         });
+//     }
+
+//     handleSubmit =  async (data, e) => {
+//         e.preventDefault();
+        
+//         const res = await postApi.postUser(data)
+//           try {
+//               const { error, ok } = res.data
+//               if(ok){
+//                   window.alert("Регистрация прошла успешно")
+//                   this.setState({
+//                     user: {
+//                         firstName: '',
+//                         login: '',
+//                         password: '',
+//                         passwordConfirm: ''
+//                     }
+//                 })
+//               } else {
+//                   window.alert(error)
+//               }
+//           }
+//            catch (err){console.log(err)}
+//     }
+
+// 	render(){
+// 		const {user:{firstName, login, password, passwordConfirm}, touched} = this.state
+// 		const warning = RegistValidation(this.state.user)
+// 		return (
+// 				<form className = {classes.Registration} action="" method="post">
+// 					<fieldset className = {classes.fieldset}> 
+// 					<legend >Регистрация</legend>
+// 					<div className = {classes.wrap}>
+// 						<Input 
+// 							label = 'Имя'
+// 							name = 'firstName'
+// 							size = 'middle'
+// 							value = {firstName}
+// 							onChange = {this.handleChange}
+// 						/>
+// 						<Input 
+// 							label = 'Логин'
+// 							name = 'login'
+// 							size = 'middle'
+// 							value = {login}
+// 							onChange = {this.handleChange}
+// 						/>
+// 						<Input 
+// 							label = 'Пароль'
+// 							type = 'password'
+// 							name = 'password'
+// 							size = 'middle'
+// 							value = {password}
+// 							onChange = {this.handleChange}
+// 						/>
+// 						<Input 
+// 							label = 'Повторите пароль'
+// 							type = 'password'
+// 							name = 'passwordConfirm'
+// 							size = 'middle'
+// 							value = {passwordConfirm}
+// 							onChange = {this.handleChange}
+// 						/>
+// 					</div>
+// 					{touched ? <span>{warning}</span> : null}
+// 					</fieldset>
+// 					<div className = {classes.wrap}>
+// 						<Button 
+// 							disabled = {!!warning}
+// 							onClick = {this.handleSubmit.bind(this, {login, firstName, password})}>
+// 							Зарегистрироваться
+// 						</Button>
+// 						<Link to ='/auth'>
+// 							<Button >Авторизация</Button>
+// 						</Link>	
+// 					</div>
+// 				</form>
+// 		)}
+// }
 
 export default Registration
 	
