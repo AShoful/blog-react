8dd202815766c486344aaa2901a7c14967b5aa68
Andrey Shoful <shoful.a@gmail.com>
2019-08-26 11:53:08 +0300

    added package material-icons-react, added icon in components/PostItem

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import Icon from 'material-icons-react';
 import  classes from './PostItem.module.css'
 
 const PostItem = ({title, createdAt, _id, removeItem, isAuth, owner:{login}}) => {
@@ -8,14 +9,22 @@ const PostItem = ({title, createdAt, _id, removeItem, isAuth, owner:{login}}) =>
 	?
 	(<p className = {classes.PostItemDate}>
 	    <span onClick = {() => removeItem(_id)}>
-	    	del
-		</span> &ensp;
+	        <Icon icon="clear" size = 'tiny' />
+	    </span> &ensp;
 		<NavLink className={classes.PostItemTitle} to={`/post/${_id}/edit`}   >
-	    	edit
+	    	<Icon icon="create" size = 'tiny' />
 		</NavLink> &ensp;
+		<span>
+			<Icon icon="perm_identity" size = 'tiny'/>
+		</span>	
+	    &ensp;
 		{new Date(createdAt).toLocaleString().split(", ")[0]}</p> )
 	:
 	(<p className = {classes.PostItemDate}>
+		<span>
+			<Icon icon="perm_identity" size = 'tiny'/>
+		</span>	
+	    &ensp;
 		{new Date(createdAt).toLocaleString().split(", ")[0]}</p>) 	
 
 	return(
